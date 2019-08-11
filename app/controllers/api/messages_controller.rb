245c26d4e9a10a0ca1973d991e8af5d4281f3a6a
ryo-fukumoto <gkrpw6767@gmail.com>
2019-08-11 11:00:25 +0900

    automatic-updating-message
    コントローラーアクションの修正

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,10 +1,10 @@
 class Api::MessagesController < ApplicationController
   def index
     # ルーティングでの設定によりparamsの中にgroup_idというキーでグループのidが入るので、これを元にDBからグループを取得する
-    group = Group.find(params[:group_id])
+    # group = Group.find(params[:group_id])
     # ajaxで送られてくる最後のメッセージのid番号を変数に代入
     # last_message_id = params[:id]
     # 取得したグループでのメッセージ達から、idがlast_messge_idよりも新しい(大きい)メッセージ達のみを取得
-    @messages = group.messages.includes(:user).where("id > ?", params[:id])
+    @messages = Message.includes(:user).where("id > ?", params[:id]).where(group_id: params[:group_id])
   end
 end
