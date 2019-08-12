88741a88b92b3dd6e5a0ceb115c7ea8435004dd0
ryo-fukumoto <gkrpw6767@gmail.com>
2019-08-12 21:31:12 +0900

    deploy
    記述の仕方を変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,9 +49,9 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
+server "3.114.31.193",
+  user: "ec2-user",
+  roles: %w{app db web}
 #   ssh_options: {
 #     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
@@ -59,5 +59,3 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-
-server '3.114.31.193', user: 'ec2-user', roles: %w{app db web}
